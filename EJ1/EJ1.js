0c6e1474127bbae0d00f1c78680f5063fa4c90f1
JoseRamon02 <1josegranada@gmail.com>
2023-02-16 16:28:20 +0100

    commit3

--- EJ1/EJ1.js
+++ EJ1/EJ1.js
@@ -5,3 +5,4 @@ let name= "John Snow";
 let admin = name;
 
 alert(admin);
+alert("Este es el administrador")
--- EJ3/EJ3.js
+++ EJ3/EJ3.js
@@ -1,3 +1,4 @@
 "use strict"
 const cumple = "18.04.1982";
 const age = someCode(cumple);
+alert ($cumple);
